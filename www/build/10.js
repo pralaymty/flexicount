27e7a056fc784c29c22c8a8d98681fe7191c9dc7
pralaymty <pralay.mty@gmail.com>
2018-11-23 18:12:37 +0530

    up2
    up2

--- www/build/10.js
+++ www/build/10.js
@@ -1,6 +1,6 @@
 webpackJsonp([10],{
 
-/***/ 337:
+/***/ 338:
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
--- www/build/7.js
+++ www/build/7.js
@@ -1,6 +1,6 @@
 webpackJsonp([7],{
 
-/***/ 347:
+/***/ 344:
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
